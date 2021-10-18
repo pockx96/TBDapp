1856d955cfa7e4d18d03b7fbe9a31f57f0e1b026
cristopher acosta <pock.kriz@gmail.com>
2021-10-18 10:21:52 -0700

    cambio de nombres , colores e iconos

--- TBDapp/TBDapp/ViewModels/AboutViewModel.cs
+++ TBDapp/TBDapp/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace TBDapp.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "Acerca de";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
--- TBDapp/TBDapp/ViewModels/ItemsViewModel.cs
+++ TBDapp/TBDapp/ViewModels/ItemsViewModel.cs
@@ -19,7 +19,7 @@ namespace TBDapp.ViewModels
 
         public ItemsViewModel()
         {
-            Title = "Browse";
+            Title = "Clientes";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
