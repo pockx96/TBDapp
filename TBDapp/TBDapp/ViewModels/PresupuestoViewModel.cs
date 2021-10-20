c553c55e80a5296f93d81267e2057aa529fe8a9f
cristopher acosta <pock.kriz@gmail.com>
2021-10-19 18:16:43 -0700

    diseño de login

---
+++ TBDapp/TBDapp/ViewModels/PresupuestoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TBDapp.ViewModels
+{
+    class PresupuestoViewModel : BaseViewModel 
+    {
+
+  
+
+     
+
+
+
+        public PresupuestoViewModel()
+        {
+            Title = "Presupuesto";
+
+
+        }
+       
+
+    }
+}
---
+++ TBDapp/TBDapp/Views/presupuestopage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TBDapp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class presupuestopage : ContentPage
+    {
+        
+        public presupuestopage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
