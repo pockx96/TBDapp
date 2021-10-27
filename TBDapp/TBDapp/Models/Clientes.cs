c079934900f65d59f502986c155fc6701b83b65c
cristopher acosta <pock.kriz@gmail.com>
2021-10-26 17:51:29 -0700

    sqlite base de datos creada

---
+++ TBDapp/TBDapp/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace TBDapp.Models
+{
+    [Table ("client")]
+    public class Clientes
+    {
+        [PrimaryKey,AutoIncrement]
+        public int id_cliente { get; set; }
+        [MaxLength (50)]
+        public string nombre_cliente { get; set; }
+    }
+}
---
+++ TBDapp/TBDapp/Models/SqliteHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace TBDapp.Models
+{
+    public class SqliteHelper
+    {
+        SQLiteAsyncConnection bd;
+
+        public SqliteHelper(string bdpath)
+        {
+            bd = new SQLiteAsyncConnection(bdpath);
+            bd.CreateTableAsync<Clientes>().Wait();
+
+
+        }
+
+       
+    }
+}
